a625759a4be7fc6ca2ccdb85f600580c39200dea
rebeccathomas-NOAA <79660143+rebeccathomas-NOAA@users.noreply.github.com>
2024-03-05 14:56:36 -0800

    fixed example

--- R/SetExpParamsFun.R
+++ R/SetExpParamsFun.R
@@ -1,13 +1,13 @@
-#'SetExpParamsFun - function to set the export parameters in Echoview given an 
+#'SetExpParamsFun - function to set the export parameters in Echoview given an
 #'Echoview export object
 #'
-#' @description Function to take an EvFile Export object and set the export parameters within it. 
+#' @description Function to take an EvFile Export object and set the export parameters within it.
 #' Parameters to export come in as ToExport. Parameters not to export come in as NoExport
-#' 
+#'
 #' @param Obj_ExpVar- an Evfile export object of the form EVfile[['Properties']][['Export']][['Variables']]
-#' 
-#' @example SetBiomassExpParamsFun(Obj_ExpVar, ToExport,NoExport)
-#' 
+#' \dontrun{
+# @example SetBiomassExpParamsFun(Obj_ExpVar, ToExport,NoExport)
+#' }
 #' @export
 SetExpParamsFun<-function(Obj_ExpVar, ToExport,NoExport){
 
